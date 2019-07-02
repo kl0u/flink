45e571c55b543ce9c5b5dbb5827b739d8d39079e
Kostas Kloudas <kkloudas@gmail.com>
2019-07-02 16:39:23 +0200

    Added lifecycle test.

--- flink-streaming-java/src/test/java/org/apache/flink/streaming/runtime/tasks/SourceTaskTerminationTest.java
+++ flink-streaming-java/src/test/java/org/apache/flink/streaming/runtime/tasks/SourceTaskTerminationTest.java
@@ -18,6 +18,7 @@
 
 package org.apache.flink.streaming.runtime.tasks;
 
+import org.apache.flink.api.common.functions.WithGracefulShutdown;
 import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
 import org.apache.flink.core.testutils.MultiShotLatch;
 import org.apache.flink.core.testutils.OneShotLatch;
@@ -27,8 +28,12 @@ import org.apache.flink.runtime.checkpoint.CheckpointType;
 import org.apache.flink.runtime.execution.CancelTaskException;
 import org.apache.flink.runtime.io.network.api.CheckpointBarrier;
 import org.apache.flink.runtime.jobgraph.OperatorID;
+import org.apache.flink.runtime.state.CheckpointListener;
 import org.apache.flink.runtime.state.CheckpointStorageLocationReference;
-import org.apache.flink.streaming.api.functions.source.SourceFunction;
+import org.apache.flink.runtime.state.FunctionInitializationContext;
+import org.apache.flink.runtime.state.FunctionSnapshotContext;
+import org.apache.flink.streaming.api.checkpoint.CheckpointedFunction;
+import org.apache.flink.streaming.api.functions.source.RichSourceFunction;
 import org.apache.flink.streaming.api.graph.StreamConfig;
 import org.apache.flink.streaming.api.operators.StreamSource;
 import org.apache.flink.streaming.api.watermark.Watermark;
@@ -38,6 +43,12 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.annotation.Nonnull;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -86,7 +97,7 @@ public class SourceTaskTerminationTest {
 	private void stopWithSavepointStreamTaskTestHelper(final boolean expectMaxWatermark) throws Exception {
 		final long syncSavepointId = 34L;
 
-		final StreamTaskTestHarness<Long> srcTaskTestHarness = getSourceStreamTaskTestHarness();
+		final StreamTaskTestHarness<Long> srcTaskTestHarness = getSourceStreamTaskTestHarness(new LockStepSourceWithOneWmPerElement());
 		final Thread executionThread = srcTaskTestHarness.invoke();
 		final StreamTask<Long, ?> srcTask = srcTaskTestHarness.getTask();
 
@@ -122,6 +133,153 @@ public class SourceTaskTerminationTest {
 		executionThread.join();
 	}
 
+	private static class LockStepSourceWithOneWmPerElement extends RichSourceFunction<Long> {
+
+		private volatile boolean isRunning;
+
+		@Override
+		public void run(SourceContext<Long> ctx) throws Exception {
+			long element = 1L;
+			isRunning = true;
+
+			ready.trigger();
+
+			while (isRunning) {
+				runLoopStart.await();
+				ctx.emitWatermark(new Watermark(element));
+				ctx.collect(element++);
+				runLoopEnd.trigger();
+			}
+		}
+
+		@Override
+		public void cancel() {
+			isRunning = false;
+			runLoopStart.trigger();
+		}
+	}
+
+	// ------------------------------------		UDF Lifecycle Monitoring Tests	------------------------------------
+
+	private final List<String> suspendOrderedFunctionCalls =
+			Collections.unmodifiableList(Arrays.asList(
+					"UDF::SNAPSHOT",
+					"UDF::CHECKPOINT-NOTIFICATION",
+					"UDF::CANCEL",
+					"UDF::CLOSE"));
+
+	private final List<String> drainOrderedFunctionCalls =
+			Collections.unmodifiableList(Arrays.asList(
+					"UDF::PREPARE-SHUTDOWN",
+					"UDF::SNAPSHOT",
+					"UDF::CHECKPOINT-NOTIFICATION",
+					"UDF::CANCEL",
+					"UDF::SHUTDOWN",
+					"UDF::CLOSE"));
+
+	private static final List<String> actualMethodCalls = new ArrayList<>();
+
+	@Test
+	public void suspendShouldNotCallGracefulShutdownMethods() throws Exception {
+		stopWithSavepointUDFLifecycleMonitoringHelper(false);
+		assertEquals(suspendOrderedFunctionCalls, actualMethodCalls);
+	}
+
+	@Test
+	public void drainShouldCallGracefulShutdownMethods() throws Exception {
+		stopWithSavepointUDFLifecycleMonitoringHelper(true);
+		assertEquals(drainOrderedFunctionCalls, actualMethodCalls);
+	}
+
+	private void stopWithSavepointUDFLifecycleMonitoringHelper(final boolean expectMaxWatermark) throws Exception {
+		actualMethodCalls.clear();
+
+		final long syncSavepointId = 34L;
+
+		final LifecycleMonitoringFunction srcFunction = new LifecycleMonitoringFunction();
+		final StreamTaskTestHarness<Long> srcTaskTestHarness = getSourceStreamTaskTestHarness(srcFunction);
+		final Thread executionThread = srcTaskTestHarness.invoke();
+		final StreamTask<Long, ?> srcTask = srcTaskTestHarness.getTask();
+
+		ready.await();
+
+		final Thread syncSavepointThread = triggerSynchronousSavepointFromDifferentThread(srcTask, expectMaxWatermark, syncSavepointId);
+
+		final SynchronousSavepointLatch syncSavepointFuture = waitForSyncSavepointFutureToBeSet(srcTask);
+
+		if (expectMaxWatermark) {
+			// if we are in TERMINATE mode, we expect the source task
+			// to emit MAX_WM before the SYNC_SAVEPOINT barrier.
+			verifyWatermark(srcTaskTestHarness.getOutput(), Watermark.MAX_WATERMARK);
+		}
+
+		verifyCheckpointBarrier(srcTaskTestHarness.getOutput(), syncSavepointId);
+
+		assertFalse(syncSavepointFuture.isCompleted());
+		assertTrue(syncSavepointFuture.isWaiting());
+
+		srcTask.notifyCheckpointComplete(syncSavepointId);
+		assertTrue(syncSavepointFuture.isCompleted());
+
+		syncSavepointThread.join();
+		executionThread.join();
+	}
+
+	private static class LifecycleMonitoringFunction extends RichSourceFunction<Long> implements WithGracefulShutdown, CheckpointedFunction, CheckpointListener {
+
+		private volatile boolean isRunning;
+
+		@Override
+		public void initializeState(FunctionInitializationContext context) throws Exception {
+
+		}
+
+		@Override
+		public void run(SourceContext<Long> ctx) throws Exception {
+			isRunning = true;
+			ready.trigger();
+
+			while (isRunning) {
+				Thread.sleep(10L);
+			}
+		}
+
+		@Override
+		public void snapshotState(FunctionSnapshotContext context) throws Exception {
+			actualMethodCalls.add("UDF::SNAPSHOT");
+		}
+
+		@Override
+		public void notifyCheckpointComplete(long checkpointId) throws Exception {
+			actualMethodCalls.add("UDF::CHECKPOINT-NOTIFICATION");
+		}
+
+		@Override
+		public void prepareToShutdown() throws Exception {
+			actualMethodCalls.add("UDF::PREPARE-SHUTDOWN");
+		}
+
+		@Override
+		public void shutdown() throws Exception {
+			actualMethodCalls.add("UDF::SHUTDOWN");
+		}
+
+		@Override
+		public void close() throws Exception {
+			super.close();
+			actualMethodCalls.add("UDF::CLOSE");
+			isRunning = false;
+		}
+
+		@Override
+		public void cancel() {
+			actualMethodCalls.add("UDF::CANCEL");
+			isRunning = false;
+		}
+	}
+
+	// ------------------------------------		Utilities	------------------------------------
+
 	private void validateNoExceptionsWereThrown() {
 		if (error.get() != null && !(error.get() instanceof CancelTaskException)) {
 			fail(error.get().getMessage());
@@ -148,6 +306,16 @@ public class SourceTaskTerminationTest {
 
 	}
 
+	private void emitAndVerifyWatermarkAndElement(
+			final StreamTaskTestHarness<Long> srcTaskTestHarness,
+			final long expectedElement) throws InterruptedException {
+
+		runLoopStart.trigger();
+		verifyWatermark(srcTaskTestHarness.getOutput(), new Watermark(expectedElement));
+		verifyNextElement(srcTaskTestHarness.getOutput(), expectedElement);
+		runLoopEnd.await();
+	}
+
 	private void emitAndVerifyCheckpoint(
 			final StreamTaskTestHarness<Long> srcTaskTestHarness,
 			final StreamTask<Long, ?> srcTask,
@@ -160,12 +328,10 @@ public class SourceTaskTerminationTest {
 		verifyCheckpointBarrier(srcTaskTestHarness.getOutput(), checkpointId);
 	}
 
-	private StreamTaskTestHarness<Long> getSourceStreamTaskTestHarness() {
+	private StreamTaskTestHarness<Long> getSourceStreamTaskTestHarness(@Nonnull final RichSourceFunction<Long> source) {
 		final StreamTaskTestHarness<Long> testHarness = new StreamTaskTestHarness<>(
 				SourceStreamTask::new,
 				BasicTypeInfo.LONG_TYPE_INFO);
-
-		final LockStepSourceWithOneWmPerElement source = new LockStepSourceWithOneWmPerElement();
 
 		testHarness.setupOutputForSingletonOperatorChain();
 		testHarness.getExecutionConfig().setLatencyTrackingInterval(-1);
@@ -187,16 +353,6 @@ public class SourceTaskTerminationTest {
 		return syncSavepointFuture;
 	}
 
-	private void emitAndVerifyWatermarkAndElement(
-			final StreamTaskTestHarness<Long> srcTaskTestHarness,
-			final long expectedElement) throws InterruptedException {
-
-		runLoopStart.trigger();
-		verifyWatermark(srcTaskTestHarness.getOutput(), new Watermark(expectedElement));
-		verifyNextElement(srcTaskTestHarness.getOutput(), expectedElement);
-		runLoopEnd.await();
-	}
-
 	private void verifyNextElement(BlockingQueue<Object> output, long expectedElement) throws InterruptedException {
 		Object next = output.take();
 		assertTrue("next element is not an event", next instanceof StreamRecord);
@@ -213,31 +369,5 @@ public class SourceTaskTerminationTest {
 		Object next = output.take();
 		assertTrue("next element is not a checkpoint barrier", next instanceof CheckpointBarrier);
 		assertEquals("wrong checkpoint id", checkpointId, ((CheckpointBarrier) next).getId());
-	}
-
-	private static class LockStepSourceWithOneWmPerElement implements SourceFunction<Long> {
-
-		private volatile boolean isRunning;
-
-		@Override
-		public void run(SourceContext<Long> ctx) throws Exception {
-			long element = 1L;
-			isRunning = true;
-
-			ready.trigger();
-
-			while (isRunning) {
-				runLoopStart.await();
-				ctx.emitWatermark(new Watermark(element));
-				ctx.collect(element++);
-				runLoopEnd.trigger();
-			}
-		}
-
-		@Override
-		public void cancel() {
-			isRunning = false;
-			runLoopStart.trigger();
-		}
 	}
 }
